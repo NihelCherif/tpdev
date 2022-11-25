85f439636816fbe050c64c75605638101c47e39f
NihelCherif <115193091+NihelCherif@users.noreply.github.com>
2022-11-25 16:22:32 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello world newwwwww!");
+        System.out.println("hello world old!");
   
     }
 }
