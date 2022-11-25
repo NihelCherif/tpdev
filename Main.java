658f6e4ceeb419b28be6f66d1b093dbb31a9b279
NihelCherif <115193091+NihelCherif@users.noreply.github.com>
2022-11-25 16:37:11 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("SUCCESS all the time!");
+        System.out.println("SUCCESS all the time !");
   
     }
 }
